8107ff0f5230c5a296c2fe6138f83041d43928d6
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2024-03-01 21:11:21 +0100

    Fix linter errors

--- chain-client/src/rest-api/GlobalRestApiConfig.ts
+++ chain-client/src/rest-api/GlobalRestApiConfig.ts
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { ContractAPI } from "@gala-chain/api";
 
 import { ContractConfig } from "../generic";
--- chain-client/src/rest-api/RestApiClientBuilder.ts
+++ chain-client/src/rest-api/RestApiClientBuilder.ts
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { ChainCallDTO, GalaChainResponse, Inferred } from "@gala-chain/api";
 import axios from "axios";
 
