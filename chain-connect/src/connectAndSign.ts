6400fd988663d0f28f01509ce3aa027d395bbc52
Sandpiper BF <befitsandpiper@protonmail.com>
2024-05-21 20:42:45 -0400

    prefix signing things, adding new files for converting to class

--- chain-connect/src/connectAndSign.ts
+++ chain-connect/src/connectAndSign.ts
@@ -15,17 +15,35 @@
 import { ChainCallDTO, signatures } from "@gala-chain/api";
 import { BrowserProvider } from "ethers";
 
+export const calculatePersonalSignPrefix = (payload: object): string => {
+  const payloadLength = signatures.getPayloadToSign(payload).length;
+  const prefix = "\u0019Ethereum Signed Message:\n" + payloadLength;
+
+  const newPayload = { ...payload, prefix };
+  const newPayloadLength = signatures.getPayloadToSign(newPayload).length;
+
+  if (payloadLength === newPayloadLength) {
+    return prefix;
+  }
+  return calculatePersonalSignPrefix(newPayload);
+};
+
 const requestAccounts = async (provider: BrowserProvider) => {
   const accounts = (await provider.send("eth_requestAccounts", [])) as string[];
 
   return accounts;
 };
 
-export const connectAndSign = async (provider: BrowserProvider, messageToSign: Buffer | object) => {
-  // TODO: if GalachainDto is used, need to use signatures.getPayloadToSign
+// NOTE: should we allow messageToSign to be a buffer?
+export const connectAndSign = async (provider: BrowserProvider, messageToSign: object) => {
+  const prefix = calculatePersonalSignPrefix(messageToSign);
+
+  const augmentedDTO = { prefix, ...messageToSign };
+  const payload = signatures.getPayloadToSign(augmentedDTO);
+
   const accounts = await requestAccounts(provider);
   const signer = await provider.getSigner();
-  const signature = await signer.provider.send("personal_sign", [accounts[0], JSON.stringify(messageToSign)]);
+  const signature = await signer.provider.send("personal_sign", [accounts[0], JSON.stringify(payload)]);
 
   return signature;
 };
