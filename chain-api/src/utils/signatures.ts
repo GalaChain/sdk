e4bdb20cdc698e6859369567769fa95b5172df9e
Koushik Gavini <koushikgavini@koushiks-macbook-pro.jerboa-altered.ts.net>
2024-07-12 12:00:28 -0700

    error hanndling

--- chain-api/src/utils/signatures.ts
+++ chain-api/src/utils/signatures.ts
@@ -144,10 +144,10 @@ function getEthAddress(publicKey: string) {
   const publicKeyBuffer = Buffer.from(publicKey, "hex");
   const keccak = keccak256.digest(publicKeyBuffer.slice(1)); // skip "04" prefix
   const addressLowerCased = Buffer.from(keccak.slice(-20)).toString("hex");
-  const checksummedAddress = checksumedEthAddress(addressLowerCased);
-  const fullAddress = checksummedAddress;
+  // const checksummedAddress = checksumedEthAddress(addressLowerCased);
+  // const fullAddress = checksummedAddress;
   // Validate the address as a final check
-  return validateEthereumAddress(fullAddress);
+  return checksumedEthAddress(addressLowerCased);
 }
 
 // the function below to calculate checksumed address is adapted from ethers.js
