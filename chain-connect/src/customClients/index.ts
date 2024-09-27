3865d965d3e4139136e576c20ea40e49bb0536f9
IndiaJonathan <indiajonathan@gmail.com>
2024-09-27 13:42:07 -0500

    missing quotes

--- chain-connect/src/customClients/index.ts
+++ chain-connect/src/customClients/index.ts
@@ -12,6 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from "./BrowserConnectClient;
+export * from "./BrowserConnectClient";
 export * from "./TrustConnectClient";
 export * from "./ServerSigningClient";
