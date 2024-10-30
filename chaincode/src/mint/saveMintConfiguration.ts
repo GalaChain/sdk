34fd553fc5978d8ee0abe3a4ec09f0b863416c13
Mike Graham <michael@forested.earth>
2024-10-30 09:20:58 -0700

    Initial TokenMintConfiguration implementation (#412)
    * wip: initial TokenMintConfiguration implementation
    
    * mike's initial changes
    
    * mint to lock features (wip)
    
    * Support pre-processing with burn-to-mint, rework post-mint lock
    
    * fix imports and exports for mint configurations
    
    * add preBurn configs to TokenMintConfiguration dto and save
    
    * add copyright to userExemptFromFees.ts
    
    * fixes for fee code base pre mint burn implementation
    
    * move burnToMint percent to TokenMintConfiguration
    
    This will support different percentages for different tokens.
    The prior implementation relied on Fee Gates and FeeCodeDefinition
    objects, which would have forced the same percentage across
    all MintToken actions configured for a pre or post mint burn.
    Moving this percentage to a token-specific chain object
    provides flexibility for different burn percentages for
    different tokens.
    
    e2e tests are currently with other still-unmigrated e2e tests
    in the internal assets-chaincode repository.
    
    * combine mint related payment failures into single error response
    
    ---------
    
    Co-authored-by: Jeff Eganhouse <jeganhouse@gala.games>
    Co-authored-by: Sandpiper <befitsandpiper@users.noreply.github.com>

---
+++ chaincode/src/mint/saveMintConfiguration.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) Gala Games Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  BurnToMintConfiguration,
+  ChainError,
+  ErrorCode,
+  PostMintLockConfiguration,
+  TokenClass,
+  TokenMintConfiguration,
+  UnauthorizedError
+} from "@gala-chain/api";
+import { plainToInstance } from "class-transformer";
+
+import { GalaChainContext } from "../types";
+import { getObjectByKey, putChainObject } from "../utils";
+
+export interface IMintConfiguration {
+  collection: string;
+  category: string;
+  type: string;
+  additionalKey: string;
+  preMintBurn?: BurnToMintConfiguration;
+  postMintBurn?: BurnToMintConfiguration;
+  postMintLock?: PostMintLockConfiguration;
+}
+
+const curatorOrgMsp = process.env.CURATOR_ORG_MSP ?? "CuratorOrg";
+
+export async function saveTokenMintConfiguration(
+  ctx: GalaChainContext,
+  data: IMintConfiguration
+): Promise<TokenMintConfiguration> {
+  const { collection, category, type, additionalKey, preMintBurn, postMintBurn, postMintLock } = data;
+
+  const existingTokenClass = await getObjectByKey(
+    ctx,
+    TokenClass,
+    TokenClass.getCompositeKeyFromParts(TokenClass.INDEX_KEY, [collection, category, type, additionalKey])
+  );
+
+  if (ctx.clientIdentity.getMSPID() !== curatorOrgMsp) {
+    if (!existingTokenClass || !existingTokenClass.authorities.includes(ctx.callingUser)) {
+      throw new UnauthorizedError(
+        `CallingUser ${ctx.callingUser} is not authorized to create or update ` +
+          `TokenMintConfiguration for ${[collection, category, type, additionalKey].join("|")} ` +
+          `${existingTokenClass ? "Authorities: " + existingTokenClass.authorities.join(", ") : ""}`
+      );
+    }
+  }
+
+  const newConfiguration = plainToInstance(TokenMintConfiguration, {
+    collection,
+    category,
+    type,
+    additionalKey,
+    preMintBurn,
+    postMintBurn
+  });
+
+  if (postMintLock !== undefined) {
+    await postMintLock.validateOrReject();
+
+    newConfiguration.postMintLock = postMintLock;
+  }
+
+  newConfiguration.validatePostProcessingTotals();
+
+  await putChainObject(ctx, newConfiguration);
+
+  return newConfiguration;
+}
