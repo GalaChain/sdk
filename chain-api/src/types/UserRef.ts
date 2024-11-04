4ca0f325bcb82d1c2a7f866f29d6dff171ea2464
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2024-11-04 18:38:06 +0100

    Add missing copyright

--- chain-api/src/types/UserRef.ts
+++ chain-api/src/types/UserRef.ts
@@ -1,12 +1,16 @@
-/**
- * @description
+/*
+ * Copyright (c) Gala Games Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Type for user alias. Technically it is a string, but it has an additional
- * marker (tag) to distinguish it from other strings at compilation level,
- * and mark it was actually validated as user alias.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should not cast any string to this type, but instead use `resolveUserAlias`
- * function to get the user alias.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 import { ValidationFailedError } from "../utils";
 import { UserRefValidationResult, meansValidUserRef, validateUserRef } from "../validators";
