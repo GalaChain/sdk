e00cdc248e57775828e0ca2afb99ac53c4502e89
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2024-11-06 08:45:14 +0100

    Remove trace field from dto

--- chain-api/src/types/dtos.ts
+++ chain-api/src/types/dtos.ts
@@ -110,11 +110,6 @@ export function createValidSubmitDTO<T extends SubmitCallDTO>(
   } as unknown as NonFunctionProperties<T>);
 }
 
-export interface TraceContext {
-  spanId: string;
-  traceId: string;
-}
-
 /**
  * @description
  *
@@ -132,9 +127,6 @@ export interface TraceContext {
  * or in the OpenAPI documentation served alongside GalaChain's API endpoints.
  */
 export class ChainCallDTO {
-  public trace?: TraceContext;
-  public static readonly ENCODING = "base64";
-
   @JSONSchema({
     description:
       "Unique key of the DTO. It is used to prevent double execution of the same transaction on chain. " +
