c3f060b19a45a9301b5369f2de07537daae1255c
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2025-04-09 17:12:13 +0200

    Add missing copyright
    Signed-off-by: Jakub Dzikowski <jakub.t.dzikowski@gmail.com>

--- chain-cli/src/__test__/data.ts
+++ chain-cli/src/__test__/data.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) Gala Games Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 export const consts = {
   developerPrivateKey: "bf2168e0e2238b9d879847987f556a093040a2cab07983a20919ac33103d0d00",
   developerPublicKey:
