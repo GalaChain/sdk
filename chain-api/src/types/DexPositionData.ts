c5ada792c203667754f2c0cefb273990bb9d8a3f
DivyanshGupta <divyansh.gupta@antiersolutions.com>
2025-05-18 20:41:28 +0530

    Change index key to make all the old pools defunct

--- chain-api/src/types/DexPositionData.ts
+++ chain-api/src/types/DexPositionData.ts
@@ -31,7 +31,7 @@ import { TokenClassKey } from "./TokenClass";
 })
 export class DexPositionData extends ChainObject {
   @Exclude()
-  static INDEX_KEY = "GCDPD"; //GalaChain Dex Position Data
+  static INDEX_KEY = "GCDXPD"; //GalaChain Decentralised Exchange Position Data
 
   @ChainKey({ position: 0 })
   @IsNotEmpty()
