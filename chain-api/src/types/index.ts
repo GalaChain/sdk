7940a0cf423b16122b77751580d92a0325afd21f
siddu0523 <sthungathurthy@gala.games>
2026-01-19 06:24:40 -0600

    Fix: Remove unused TokenSale feature (#729)

--- chain-api/src/types/index.ts
+++ chain-api/src/types/index.ts
@@ -67,7 +67,6 @@ export * from "./OracleBridgeFeeAssertion";
 export * from "./OracleDefinition";
 export * from "./OraclePriceAssertion";
 export * from "./OraclePriceCrossRateAssertion";
-export * from "./sale";
 export * from "./swap";
 export * from "./TokenMintAllowance";
 export * from "./TokenMintAllowanceRequest";
