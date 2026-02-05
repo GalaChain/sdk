8f789798303d7002000f40fa692054bbe14a0e18
Jakub Dzikowski <jakub.t.dzikowski@gmail.com>
2026-02-05 12:35:45 +0100

    Chore: Get version utility (#765)

---
+++ chaincode/src/utils/version.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Gala Games Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+let version = "0.0.0";
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  version = require("../../../package.json").version;
+} catch (e) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  version = require("../../package.json").version;
+}
+
+/**
+ * Returns the current package version of the chaincode.
+ * @returns The version string of the package.json file.
+ */
+export function getVersion(): string {
+  return version;
+}
